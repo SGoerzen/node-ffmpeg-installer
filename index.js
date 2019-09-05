0aacdb049ce1dde3d8a31da6b831874d5dc3cbf0
Sergej Görzen <sergej.goerzen@rwth-aachen.de>
2019-09-05 13:38:20 +0200

    done core features

---
+++ index.js
@@ -0,0 +1,100 @@
+const { exec, execSync } = require('child_process');
+const downloadsFolder = require('downloads-folder');
+const { download, install } = require('downinst');
+const path = require('path');
+const fs = require('fs');
+const extract = require('extract-zip');
+
+function isInstalled() {
+    return new Promise((resolve, reject) => {
+        exec('ffmpeg -version', (err, stdout, stderr) => {
+            err ? reject() : resolve();
+        });
+    });
+}
+
+function isInstalledSync() {
+    try {
+        execSync('ffmpeg -version');
+        return true;
+    }
+    catch {
+        return false;
+    }
+}
+
+function extractFFMPEG(zipFile, target, onFinish) {
+    extract(zipFile, {dir: target}, (err) => {
+        if (err) throw err;
+        fs.readdir(target, (err, files) => {
+            if (err) throw err;
+            files.forEach(function (extractedFolder) { 
+                if (onFinish)
+                    onFinish(path.join(target, extractedFolder));
+            });
+        });
+    });
+}
+
+function installFFMPEG(onFinish, onDownload, installPlace) { 
+    
+    if (process.platform === "win32") {
+        const outFile = path.join(downloadsFolder(), "ffmpeg.zip");
+        // TODO auto assign version
+        download({
+            type: "http",
+            out: outFile,
+            files: {
+                "win32": {
+                    "x64": "https://ffmpeg.zeranoe.com/builds/win64/static/ffmpeg-20190826-0821bc4-win64-static.zip",
+                    "x86": "https://ffmpeg.zeranoe.com/builds/win32/static/ffmpeg-20190826-0821bc4-win32-static.zip"
+                }
+            }
+        }, {
+            onFinish: (file) => {
+                const target = path.join(downloadsFolder(), 'ffmpeg');
+                extractFFMPEG(file, target, (extracted) => {
+                    if (fs.existsSync(installPlace)) {
+                        deleteFolderRecursive(installPlace);
+                    }
+                    fs.rename(extracted, installPlace, (err) => {
+                        if (err) throw err;
+                        if (onFinish)
+                            onFinish();
+                    });
+                });
+            },
+            onProgress: onDownload
+        });
+    } else {
+        throw new Error("Not implemented yet");
+        // TODO FOR DARWIN AND LINUX
+    }
+    
+    
+}
+
+function update() {
+    // TODO
+}
+
+function deleteFolderRecursive(path) {
+    if( fs.existsSync(path) ) {
+        fs.readdirSync(path).forEach(function(file,index){
+        var curPath = path + "/" + file;
+        if(fs.lstatSync(curPath).isDirectory()) { // recurse
+            deleteFolderRecursive(curPath);
+        } else { // delete file
+            fs.unlinkSync(curPath);
+        }
+        });
+        fs.rmdirSync(path);
+    }
+}
+
+module.exports = {
+    isInstalled: isInstalled,
+    isInstalledSync: isInstalledSync,
+    install: installFFMPEG,
+    extract: extractFFMPEG
+};
